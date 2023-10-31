3b6803a30f65d231d441aae9979ac4be810e5ac3
sem <sem@gmail.com>
2023-10-31 20:33:25 +0500

    todonotes

---
+++ src/components/AddNote.vue
@@ -0,0 +1,15 @@
+<template>
+    <button class="addNote">
+        <img src="@/assets/images/edit.svg" alt="">
+    </button>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
